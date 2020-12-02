c27014dada7e7a2c6718f4e6fcb72e084ee4896a
Iorjunior <juniorcoda@gmail.com>
2020-12-02 15:33:14 -0300

    setup inicial!

---
+++ app.py
@@ -0,0 +1,11 @@
+from tkinter import *   
+
+class Application():
+    def __init__(self,master = None)
+    self.master = master
+
+
+if __name__ == "__main__":
+    root = Tk()
+    app = Application(root)
+    root.mainloop()
